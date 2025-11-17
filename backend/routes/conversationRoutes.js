fa508435fed52533acf8d81b4c84249f559b09f4
nathira99 <nathirafarveen99@gmail.com>
2025-11-18 05:18:55 +0530

    redirect --fix

--- backend/routes/conversationRoutes.js
+++ backend/routes/conversationRoutes.js
@@ -10,7 +10,7 @@ const {
 // GET recent chats
 router.get("/", protect, getConversations);
 router.get("/recent", protect, getRecentChats);
-router.post("/start", protect, startChat);
+router.post("/conversations", protect, startChat);
 
 
 module.exports = router;
