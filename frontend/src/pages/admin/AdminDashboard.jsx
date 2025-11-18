33c990732944fd0f353d72e18cfbede8c5c56c25
nathira99 <nathirafarveen99@gmail.com>
2025-11-18 16:00:17 +0530

    ui --fix

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -6,7 +6,7 @@ export default function AdminDashboard() {
   return (
     <>
       <Navbar className="fixed top-0 left-0 right-0" />
-      <div className="flex fixed top-16 left-0 right-0 h-svh max-sm:flex-col sm:fixed sm:top-16 sm:left-0 sm:right-0 items-center justify-center">
+      <div className="dark:bg-gray-900 dark:text-gray-200 flex fixed top-16 left-0 right-0 h-svh max-sm:flex-col sm:fixed sm:top-16 sm:left-0 sm:right-0 items-center justify-center">
       <AdminSidebar />
       <main className="flex-1 p-6 overflow-y-scroll">
         <Outlet />
