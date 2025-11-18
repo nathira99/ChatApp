99250f2e26f0dc9b6ef2a54fd6e13d2c1caaea6a
nathira99 <nathirafarveen99@gmail.com>
2025-11-18 16:04:57 +0530

    ui --fix

--- frontend/src/pages/admin/AdminReports.jsx
+++ frontend/src/pages/admin/AdminReports.jsx
@@ -134,7 +134,7 @@ export default function AdminReports() {
           return (
             <div
               key={r._id}
-              className="bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-xl p-4 shadow-sm"
+              className="bg-white dark:bg-gray-800 dark:text-gray-200 border border-gray-300 dark:border-gray-700 rounded-xl p-4 shadow-sm"
             >
               {/* Report details */}
               <p className="text-sm text-gray-700 dark:text-gray-200">
